d8792b14a1db094351279fc2e6982451c3d3ea17
Jamie Webster <jamie_maxwell_webster@hotmail.com>
2015-11-28 19:06:52 -0500

    Created dota client with conect + download replay Added primary test case

---
+++ src/TimeoutHandler.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace HGV.Crystalys
+{
+	public class TimeoutHandler
+	{
+		public static bool RetryUntilSuccessOrTimeout(TimeSpan timeSpan, Func<bool> task)
+		{
+			bool success = false;
+			int elapsed = 0;
+			while ((!success) && (elapsed < timeSpan.TotalMilliseconds))
+			{
+				Thread.Sleep(1000);
+				elapsed += 1000;
+				success = task();
+			}
+			return success;
+		}
+
+	}
+}
---
+++ tests/HGV.Crystalys.Tests/DotaClientTest.cs
@@ -0,0 +1,69 @@
+﻿using SteamKit2.GC;
+using SteamKit2.GC.Dota.Internal;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace HGV.Crystalys.Tests
+{
+	public class DotaClientTest
+	{
+		// Mollie Burton
+		// thantsking@outlook.com
+		// "Thantsking" / "aPhan3sah"
+		// 1962101529
+
+		private string Username = "Thantsking";
+		private string Password = "aPhan3sah";
+
+		[Fact]
+		public void ConnectToSteam()
+		{
+			Assert.True(File.Exists("sentry.bin"));
+
+			// if we have a saved sentry file, read 
+			byte[] sentryFile = File.ReadAllBytes("sentry.bin");
+
+			using (var client = new DotaClient(this.Username, this.Password, sentryFile))
+			{
+				client.OnConnected += (o, e) => {
+					if(e.Result)
+						Trace.TraceInformation("Connected to Steam, Logging in '{0}'", this.Username);
+					else
+						Trace.TraceError("Unable to connect to Steam");
+				};
+				client.OnDisconnected += (o, e) => {
+					e.Reconnect = true;
+
+					Trace.TraceInformation("Disconnected from Steam.");
+				};
+				client.OnLoggedOn += (o, e) => {
+					if(e.Sucess)
+						Trace.TraceInformation("Successfully logged on!");
+					else
+						Trace.TraceError("Unable to logon to Steam: {0}", e.Result);
+				};
+				client.OnAccountLogonDenied += (o, e) => {
+					Trace.TraceInformation("Auth code sent to the email at {0} is required. Regenerate Sentry File Hash.", e.EmailDomain);
+				};
+				client.OnLoggedOff += (o, e) => {
+					Trace.TraceInformation("Logged off of Steam");
+				};
+				client.OnClientWelcome += (o, e) => {
+					Trace.TraceInformation("GC is welcoming us. Version: {0}", e.Message.version);
+				};
+
+				client.Connect(TimeSpan.FromMinutes(1));
+
+				var data = client.DownloadReplay(1962101529, TimeSpan.FromMinutes(1));
+
+				Assert.NotEqual(0, data.Length);
+			}
+        }
+	}
+}
