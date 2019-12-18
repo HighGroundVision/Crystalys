cf5260053fe2b8b30171cb0e2d55f94eff943d7b
Jamie Webster <jamie.webster@factorybucket.com>
2019-12-18 13:53:10 -0500

    Stuff and Things

---
+++ src/SteamConnection.cs
@@ -0,0 +1,67 @@
+﻿using SteamKit2;
+using SteamKit2.Internal;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+
+namespace HGV.Crystalys
+{
+    public interface ISteamAuthenticationProvider
+    {
+        string GetUserName();
+        string GetPassword();
+    }
+
+    public interface ISteamConnection
+    {
+        bool isConnected();
+        SteamClient GetClient();
+    }
+
+    public class SteamConnection : ISteamConnection
+    {
+        protected readonly SteamClient client;
+        private readonly ISteamAuthenticationProvider provider;
+
+        public SteamConnection(ISteamAuthenticationProvider provider)
+        {
+            this.provider = provider;
+            this.client = new SteamClient();
+
+            this.Connect();
+        }
+
+        private void Connect()
+        {
+            var user = this.client.GetHandler<SteamUser>();
+            var callbacks = new CallbackManager(this.client);
+            
+            callbacks.Subscribe<SteamClient.ConnectedCallback>(_ => {
+                var details = new SteamUser.LogOnDetails
+                {
+                    Username = this.provider.GetUserName(),
+                    Password = this.provider.GetPassword(),
+                };
+                user.LogOn(details);
+            });
+            callbacks.Subscribe<SteamUser.LoggedOnCallback>(_ => {
+                // Do Nothing
+            });
+
+            this.client.Connect();
+
+            callbacks.RunWaitAllCallbacks(TimeSpan.FromSeconds(5));
+        }
+
+        public bool isConnected()
+        {
+            return this.client.IsConnected;
+        }
+
+        public SteamClient GetClient()
+        {
+            return this.client;
+        }
+    }
+}
